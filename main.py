91f99924bc1a311c8126f22a297ab47f13098473
krypton <52121207+krypton-byte@users.noreply.github.com>
2020-08-23 13:35:09 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import *
+import dj
+import urllib, os
+app = Flask(__name__)
+app.config['MEDIA'] = 'pdf'
+@app.route('/unduh/<path:filename>',methods=['GET','POST'])
+def unduh(filename):
+	return send_from_directory(app.config['MEDIA'], filename, as_attachment=True)
+@app.route('/',methods=['GET','POST'])
+def index():
+	return "Hallo World"
+@app.route('/doujinshi/<path:nuklir>',methods=['GET','POST'])
+def DownConverter(nuklir):
+	try:
+		judul=dj.parser(nuklir)
+		return {
+			'status':True,
+			'content':'/unduh/%s'%(urllib.parse.quote(judul))
+			}
+	except IndexError:
+		return {
+			'status':False,
+			'pesan':'kode nulir salah'
+			}
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port=int(os.environ.get('PORT','5000')), debug=True)
